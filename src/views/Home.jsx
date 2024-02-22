f3077bc851f7ef95d9b96241d46cb71d77f142cf
RatebBarakat <ratebbarakat2021@gmail.com>
2024-02-22 16:47:25 +0200

    change design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,37 +1,31 @@
 import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../contexts/auth";
-import { useNavigate } from "react-router-dom";
 import Loading from "../components/Loading";
 import createAxiosInstance from "../axios";
 import Toast from "../components/Toast";
 import { CartContext } from "../contexts/cart";
+import Header from "../components/Header";
+import { AuthContext } from "../contexts/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const auth = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const axios = createAxiosInstance();
   const cart = useContext(CartContext);
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('/user/google/redirect').then(r => console.log(r)).catch(e => {
-      console.warn(e);
-    })
-    axios.get("/product").then((response) => {
-      setProducts(response.data.data);
-      setIsLoading(false);
-    });
-    axios.get("/admin/categories").then((response) => {
-      setProducts(response.data.data);
+    axios.get("/user/products/home").then((response) => {
+      setCategories(response.data);
       setIsLoading(false);
     });
   }, []);
 
   const addToCart = async (productId) => {
-    await cart.addToCart(productId);
-    await cart.fetchCart();
-    Toast.notifyMessage("success", "added");
+    cart.addToCart(productId).catch(() => {
+      navigate("/login");
+    });
   };
 
   return (
@@ -40,53 +34,63 @@ export default function Home() {
         <Loading centered={false} size={7} />
       ) : (
         <>
-          <h1 className="text-2xl font-semibold mb-4">
-            Welcome, you cart contains {cart.carts.length}
-          </h1>
-          <h2 className="text-xl font-semibold mb-2">Products:</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {products.map((product) => (
+          <Header AuthContext={auth} CartContext={cart} />
+          <div className="cotainer p-2 border shadow-lg bg-gray-100">
+            {categories.map((category) => (
               <div
-                key={product.id}
-                className="bg-white rounded-lg shadow-lg p-4"
+                key={category.category.name}
+                className="shadow mb-2 rounded pb-2 bg-gray-50"
               >
-                <h3 className="text-xl font-semibold">{product.name}</h3>
-                <p className="text-gray-500 my-2">
-                  {product.small_description}
-                </p>
-                <div className="flex justify-between items-center mt-4">
-                  <div>
-                    <p className="text-green-500 font-semibold text-xl">
-                      ${product.price}
-                    </p>
-                    <p className="text-gray-500">
-                      Quantity: {product.quantity}
-                    </p>
-                  </div>
-                  <button
-                    className={`text-white py-2 px-4 rounded-lg ${
-                      cart.isInCart(product.id)
-                        ? "!bg-indigo-300 cursor-not-allowed"
-                        : "bg-indigo-600"
-                    } hover:bg-indigo-700`}
-                    onClick={() => addToCart(product.id)}
-                  >
-                    {cart.isLoading ? <Loading size={5} /> : 'Add to Cart'}
-                  </button>
-                </div>
-                <div className="mt-4">
-                  <p className="text-gray-500 font-medium">Tags:</p>
-                  <div className="flex space-x-2">
-                    {product.tags &&
-                      product.tags.split(",").map((tag, index) => (
-                        <span
-                          key={index}
-                          className="bg-gray-200 text-gray-700 rounded-full py-1 px-2 text-sm"
-                        >
-                          {tag}
-                        </span>
-                      ))}
-                  </div>
+                <h2 className="text-md text-center text-indigo-600 font-bold border mb-2 rounded py-2">
+                  {category.category.name}
+                </h2>
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center">
+                  {category.category.products.map((product) => (
+                    <div
+                      key={product.id}
+                      className="product shadow border bg-white rounded-lg mx-auto w-full overflow-hidden"
+                    >
+                      <img
+                        src={product.image}
+                        alt="no image"
+                        className="w-full h-40 object-cover mb-4"
+                      />
+                      <div className="content px-2">
+                        <h3 className="text-lg font-semibold mb-1">
+                          {product.name}
+                        </h3>
+                        <p className="text-gray-600 mb-1">
+                          {product.smallDescription}
+                        </p>
+                        <p className="text-gray-800 font-semibold">
+                          ${product.price}
+                        </p>
+                        {cart.isInCart(product.id) ? (
+                          <button
+                            onClick={() => addToCart(product.id)}
+                            disabled
+                            className="bg-indigo-200 text-white px-2 py-1 rounded-md mb-2"
+                          >
+                            In Cart
+                          </button>
+                        ) : cart.isLoading ? (
+                          <button
+                            disabled
+                            className="bg-indigo-300 text-white px-2 py-1 rounded-md mb-2"
+                          >
+                            loading...
+                          </button>
+                        ) : (
+                          <button
+                            onClick={() => addToCart(product.id)}
+                            className="bg-indigo-600 text-white px-2 py-1 rounded-md mb-2"
+                          >
+                            Add to Cart
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             ))}
